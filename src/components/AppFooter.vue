e04a63ab6c77e49b29f146c55b305fe86c589b10
Robertarezza <roberta.rezza@gmail.com>
2024-05-02 17:39:37 +0200

    add: components footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,18 +1,142 @@
-<script >
+<script>
+
 
 
 export default {
+    components:{
+       
+    },
   data() {
-    return {};
+    return {
+        menuFooter: [
+            {
+                title: "Characters",
+            },
+            {
+                title: "Comics",
+            },
+            {
+                title: "Movies",
+                
+            },
+            {
+                title: "Tv",
+               
+            },
+            {
+                title: "Games",
+                
+            },
+           
+            {
+                title: "Videos",
+          
+            },
+           
+            {
+                title: "News",
+               
+            },
+            
+        ],
+        menuShop: [
+            {
+                title: "Shop DC ",
+            },
+            {
+                title: "Shop DC Collectibles",
+            },
+        ],
+    };
   },
 };
 </script>
 
-
 <template>
-  <h1>Footer</h1>
+  <footer>
+    <div class="footer">
+    <div class="content-footer-sx">
+        <section>
+        <h3>DC COMICS</h3>
+        <ul>
+            <li v-for='item in menuFooter'><a href="" >{{item.title}}</a></li>
+        </ul>
+        <h3>SHOP</h3>
+        <ul>
+            <li v-for='item in menuShop'><a href="" >{{item.title}}</a></li>
+        </ul>
+      </section>
+      <section>
+        <h3>DC</h3>
+        <ul>
+            <li v-for='item in menuFooter'><a href="" >{{item.title}}</a></li>
+        </ul>
+        
+      </section>
+      <section>
+        <h3>SITES</h3>
+        <ul>
+            <li v-for='item in menuFooter'><a href="" >{{item.title}}</a></li>
+        </ul>
+      </section>
+    </div>
+    <div class="content-footer-dx">
+        <section class="immag-footer">
+       <img src="../assets/img/dc-logo.png" alt=""> 
+      </section>
+    </div>
+      
+    </div>
+  </footer>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/_mixin.scss" as *;
 
+.footer{
+
+    @include flex(row, flex-start, flex-start);
+    background-image: url(../assets/img/footer-bg.jpg);
+
+
+.content-footer-sx {
+  padding: 5rem 0;
+  display: flex;
+  flex-direction: row;
+  
+
+  h3{
+    color: white;
+    margin: 0;
+    @include flex(column, flex-start, flex-start);
+    padding: 0 5px;
+   
+  }
+
+  ul{
+    list-style-type: none;
+    padding: 0;
+    margin:0 5px;
+    @include flex(column, flex-start, flex-start);
+
+    a{
+        text-decoration: none;
+        color: #ffffff40;
+        font-size: 0.8rem;
+       
+    }
+}
+
+section{
+    @include flex(column, flex-start, flex-start);
+   margin:0 5px;
+  
+  .immag-footer{
+    flex-direction: flex-and;
+   }
+
+}
+
+}
+}
 </style>
