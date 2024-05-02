62a73d16ce54276c24dbcf6285aa4a3039b5dc6a
Robertarezza <roberta.rezza@gmail.com>
2024-05-02 15:52:08 +0200

    add:AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,16 +3,98 @@
 
 export default {
   data() {
-    return {};
+    return {
+        menuHeader: [
+            {
+                title: "Characters",
+                isActive: false
+            },
+            {
+                title: "Comics",
+                isActive: true
+            },
+            {
+                title: "Movies",
+                isActive: false
+            },
+            {
+                title: "Tv",
+                isActive: false
+            },
+            {
+                title: "Games",
+                isActive: false
+            },
+            {
+                title: "Collectibles",
+                isActive: false
+            },
+            {
+                title: "VIDEOS",
+                isActive: false
+            },
+            {
+                title: "FANS",
+                isActive: false
+            },
+            {
+                title: "NEWS",
+                isActive: false
+            },
+            {
+                title: "SHOP",
+                isActive: false
+            },
+        ]
+    };
   },
 };
 </script>
 
 
 <template>
-  <h1>Header</h1>
+<header>
+<div class="header-logo">
+    <img src="../assets/img/dc-logo.png" alt="">
+</div>
+   
+    <ul class="navigation">
+        
+        <li v-for='item in menuHeader'><a href="" :class="{active: item.isActive}">{{item.title}}</a></li>
+       
+    </ul>
+</header>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/_mixin.scss" as *;
+@use "../style/partials/_variables.scss" as *;
+
+
+
+header{
+    @include flex(row, space-around, center);
+    padding: 15px 0;
+
+    ul{
+        @include flex(row, center, flex-end);
+        list-style-type: none;
+        gap: 1rem;
+
+        a{
+            text-decoration: none;
+            color: $text-color;
+            // font-size: 1.5rem;
+            text-transform: uppercase;
+            padding-bottom:50px;
+
+            &.active {
+                color: $active-color;
+                border-bottom:2px solid $active-color;
+             
+            }
+        }
+    }
+}
 
 </style>
